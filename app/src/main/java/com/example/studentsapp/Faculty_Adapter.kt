0bb13113e8ca8f50180d841d7008f4cea00e217f
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-05-24 16:16:46 +0530

    Commit

--- app/src/main/java/com/example/studentsapp/Faculty_Adapter.kt
+++ app/src/main/java/com/example/studentsapp/Faculty_Adapter.kt
@@ -3,25 +3,30 @@ package com.example.studentsapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class Faculty_Adapter(private val facultylist:ArrayList<department_card>):RecyclerView.Adapter<Faculty_Adapter.faculty_viewholder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): faculty_viewholder {
-        val itemView=LayoutInflater.from(parent.context).inf
+        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.faculty_card,parent,false)
+        return faculty_viewholder(itemView)
     }
 
     override fun onBindViewHolder(holder: faculty_viewholder, position: Int) {
-        TODO("Not yet implemented")
+        val currentitem=facultylist[position]
+        holder.f_name.text=currentitem.faculty_name
+        holder.f_name.text=currentitem.faculty_desgn
     }
 
     override fun getItemCount(): Int {
-        facultylist.size
+        return facultylist.size
     }
 
 
     class faculty_viewholder(itemView:View):RecyclerView.ViewHolder(itemView){
 
-
+        val f_name=itemView.findViewById<TextView>(R.id.f_name)
+        val f_desgn=itemView.findViewById<TextView>(R.id.f_desgn)
     }
 }
 
