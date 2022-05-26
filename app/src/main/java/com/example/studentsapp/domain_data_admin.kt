744ec956f744ba00169fb8595401a8124644e0fb
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-05-26 15:43:46 +0530

    Commit

---
+++ app/src/main/java/com/example/studentsapp/domain_data_admin.kt
@@ -0,0 +1,3 @@
+package com.example.studentsapp
+
+data class domain_data_admin(val domain_desc:String?=null)
--- app/src/main/java/com/example/studentsapp/non_academic.kt
+++ app/src/main/java/com/example/studentsapp/non_academic.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_non_academic.*
+import kotlinx.android.synthetic.main.fragment_non_academic.view.*
 
 
 class non_academic : Fragment() {
@@ -19,6 +22,38 @@ class non_academic : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view= inflater.inflate(R.layout.fragment_non_academic, container, false)
+        view.RS.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(Rs_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.ENIGMA.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(E_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.IIC.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(iic_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.vc.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(vc_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.dance.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(vib_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.souls.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(sou_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.sports.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(spo_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
+        view.samavesh.setOnClickListener {
+            val action=non_academicDirections.actionNonAcademicToNonTechDesc(sam_t.text.toString())
+            Navigation.findNavController(view).navigate(action)
+        }
 
         return view
     }
