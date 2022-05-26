d40bf33780b406fb252b73ddf9867dd0236c656a
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-05-26 12:09:42 +0530

    Notice Page Commit

--- app/src/main/java/com/example/studentsapp/Notice_Adapter.kt
+++ app/src/main/java/com/example/studentsapp/Notice_Adapter.kt
@@ -3,22 +3,23 @@ package com.example.studentsapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.File
 
-class Notice_Adapter(private val noticelist:ArrayList<Notice_card>):RecyclerView.Adapter<Notice_Adapter.ViewHolder>() {
+
+class Notice_Adapter(private val noticelist:ArrayList<Notice_card>, private val click:OnItemClickListener):RecyclerView.Adapter<Notice_Adapter.ViewHolder>() {
 
     val fileref=Firebase.storage.reference
 
+    interface OnItemClickListener{
+        fun onItemClick(item:String)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView=LayoutInflater.from(parent.context).inflate(R.layout.notice_card,
         parent,false)
@@ -29,7 +30,9 @@ class Notice_Adapter(private val noticelist:ArrayList<Notice_card>):RecyclerView
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentitem=noticelist[position]
         holder.notice_title.text=currentitem.noticetitle
-
+        holder.dwnld_btn.setOnClickListener {
+            click.onItemClick(noticelist.get(position).imgUrl!!)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -38,7 +41,7 @@ class Notice_Adapter(private val noticelist:ArrayList<Notice_card>):RecyclerView
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
 
         val notice_title=itemView.findViewById<TextView>(R.id.Notice_title)
-
+        val dwnld_btn=itemView.findViewById<ImageButton >(R.id.dwnldButton)
     }
 
 }
--- app/src/main/java/com/example/studentsapp/home.kt
+++ app/src/main/java/com/example/studentsapp/home.kt
@@ -1,30 +1,39 @@
 package com.example.studentsapp
 
+import android.app.DownloadManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.checkSelfPermission
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.*
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.jar.Manifest
+import android.hardware.SensorManager
 
 
-class home : Fragment() {
+
+
+
+class home : Fragment(),Notice_Adapter.OnItemClickListener {
 
     private lateinit var dbref:DatabaseReference
     private lateinit var notice_recyclerview:RecyclerView
     private lateinit var noticelist:ArrayList<Notice_card>
     var dataurl:String=""
+    var dataUri:ArrayList<Uri> = ArrayList()
+    private val REQ_CODE=100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -50,7 +59,6 @@ class home : Fragment() {
         return view
 
     }
-
     private fun getnoticedata() {
         dbref = FirebaseDatabase.getInstance().getReference("Notices")
 
@@ -61,7 +69,7 @@ class home : Fragment() {
                         val user = userSnapshot.getValue(Notice_card::class.java)
                         noticelist.add(user!!)
                     }
-                    notice_recyclerview.adapter = Notice_Adapter(noticelist)
+                    notice_recyclerview.adapter = Notice_Adapter(noticelist,this@home)
                 }
             }
             override fun onCancelled(error: DatabaseError) {
@@ -70,5 +78,41 @@ class home : Fragment() {
         })
 
     }
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onItemClick(item: String) {
+        dataurl=item
+        if(context?.let { checkSelfPermission(it,android.Manifest.permission.WRITE_EXTERNAL_STORAGE) } ==PackageManager.PERMISSION_DENIED){
+            requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),REQ_CODE)
+        }else{
+            startDownloading()
+        }
+            Toast.makeText(activity?.applicationContext,"You Clicked on Download Button",Toast.LENGTH_SHORT).show()
+    }
+    private fun startDownloading(){
+        val request=DownloadManager.Request(Uri.parse(dataurl))
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        request.setTitle("Download")
+        request.setDescription("Your File is Downloading >>>")
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"${System.currentTimeMillis()}")
+        val Manager = activity!!.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        Manager.enqueue(request)
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQ_CODE->{
+                if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    startDownloading()
+                }else{
+                    Toast.makeText(activity?.applicationContext,"Permission not Granted",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 }
